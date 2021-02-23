ef71f4f1b5f7363e7f7b70f6c61d53313ce6ba5a
Mateus Fugita <mateushiwao@gmail.com>
2021-02-22 22:00:02 -0300

    create initial server structure

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({
+        message: 'Hello World!'
+    });
+})
+
+app.post('/', (request, response) => {
+    return response.json({
+        message: 'Os dados foram salvos com sucesso!'
+    });
+})
+
+app.listen(3333, () => console.log('Server is running!'));
+
+//yarn add typescript -D
+//yarn tsc --init
+//yarn add ts-node-dev -D
